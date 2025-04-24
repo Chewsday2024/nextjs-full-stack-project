b6de5b831304c304d5aacf465c0fc182b84b6a4e
Chewsday <z647623@gmail.com>
2025-04-24 21:29:56 +0800

    stage 7 done

--- src/app/(rs)/tickets/form/TicketForm.tsx
+++ src/app/(rs)/tickets/form/TicketForm.tsx
@@ -15,13 +15,20 @@ import { selectCustomerSchemaType } from "@/zod-schemas/customer";
 
 type Props = {
   customer: selectCustomerSchemaType,
-  ticket?: selectTicketSchemaType
+  ticket?: selectTicketSchemaType,
+  techs?: {
+    id: string,
+    description: string
+  }[],
+  isEditable?: boolean
 }
 
 
 export default function TicketForm({
-  customer, ticket
+  customer, ticket, techs, isEditable = true
 }: Props) {
+  const isManager = Array.isArray(techs);
+
   const defaultValues: insertTicketSchemaType = {
     id: ticket?.id ?? '(New)',
     customerId: ticket?.customerId ?? customer.id,
@@ -48,7 +55,12 @@ export default function TicketForm({
     <div className="flex flex-col gap-1 sm:px-8">
       <div>
         <h2 className="text-2xl font-bold">
-          {ticket?.id ? 'Edit' : 'New'} Ticket {ticket?.id ? `# ${ticket.id}` : 'Form'}
+          {ticket?.id && isEditable
+            ? `Edit Ticket # ${ticket.id}` 
+            : ticket?.id 
+              ? `View Ticket # ${ticket.id}`
+              : 'New Ticket Form'
+          }
         </h2>
       </div>
 
@@ -61,19 +73,36 @@ export default function TicketForm({
             <InputWithLabel<insertTicketSchemaType>
               fieldTitle='Title'
               nameInSchema="title"
+              disabled={!isEditable}
             />
 
-            <InputWithLabel<insertTicketSchemaType>
-              fieldTitle='Tech'
-              nameInSchema="tech"
-              disabled={true}
-            />
 
-            <CheckboxWithLabel<insertTicketSchemaType>
-              fieldTitle='Completed'
-              nameInSchema='completed'
-              message="Yes"
-            />
+            {isManager ? (
+              <SelectWithLabel<insertTicketSchemaType>
+                fieldTitle="Tech ID"
+                nameInSchema="tech"
+                data={[{ id: 'new-ticket@example.com', description: 'new-ticket@example.com'}, ...techs]}
+              />
+            ) : (
+              <InputWithLabel<insertTicketSchemaType>
+                fieldTitle='Tech'
+                nameInSchema="tech"
+                disabled={true}
+              />
+            )}
+            
+
+            {ticket?.id ? (
+              <CheckboxWithLabel<insertTicketSchemaType>
+                fieldTitle='Completed'
+                nameInSchema='completed'
+                message="Yes"
+                disabled={!isEditable}
+              />
+            ) : (
+              null
+            )}
+            
 
 
             <div className="mt-4 space-y-2">
@@ -99,27 +128,34 @@ export default function TicketForm({
               fieldTitle='Description'
               nameInSchema="description"
               className="h-96"
+              disabled={!isEditable}
             />
 
-            <div className="flex gap-2">
-              <Button
-                type="submit"
-                className="w-3/4"
-                variant='default'
-                title="Save"
-              >
-                Save
-              </Button>
 
-              <Button
-                type="button"
-                variant='destructive'
-                title="Reset"
-                onClick={() => form.reset(defaultValues)}
-              >
-                Reset
-              </Button>
-            </div>
+            {isEditable ? (
+              <div className="flex gap-2">
+                <Button
+                  type="submit"
+                  className="w-3/4"
+                  variant='default'
+                  title="Save"
+                >
+                  Save
+                </Button>
+
+                <Button
+                  type="button"
+                  variant='destructive'
+                  title="Reset"
+                  onClick={() => form.reset(defaultValues)}
+                >
+                  Reset
+                </Button>
+              </div>
+            ) : 
+              null
+            }
+            
           </div>
           
         </form>
